c882e569eee457d4e3c561f391a82d077c985960
Simon Stensgaard <STcena1@hotmail.com>
2026-01-05 13:17:47 +0100

    Added _handle_input() method to BaseReder.py

--- src/reader/BaseReader.py
+++ src/reader/BaseReader.py
@@ -10,6 +10,7 @@ from src.utility import retrieve_output_format
 
 
 from typing import List, Union, Optional, Tuple, Any, Dict
+from io import StringIO, BytesIO
 from abc import abstractmethod
 from dataclasses import field
 from datetime import datetime, timezone
@@ -18,28 +19,43 @@ from datetime import datetime, timezone
 # BASEREADER CLASS -> ABSTRACTION
 # ---------------------------------------------------------------
 
+
+
 class BaseReader():
 
-    __slots__ = ("output_format", "collect_metadata", "history")
+    __slots__ = ("output_engine", "collect_metadata", "history")
 
     def __init__(
         self,
-        output_format: str = "pandas",
+        output_engine: str = "pandas",
         collect_metadata: bool = True,
     ):
-        self.output_format = field(default_factory=retrieve_output_format(input=output_format))
+        self.output_format = field(default_factory=retrieve_output_format(input=output_engine))
         self.collect_metadata = collect_metadata
         self.history = []
 
-
     def read(
         self,
-        input: str = None,
+        input: str | StringIO | BytesIO,
     ) -> pd.DataFrame | pl.DataFrame | pa.Table:
         pass
 
+    def _handle_input(
+        self,
+        input: str | StringIO | BytesIO
+    ) -> str | StringIO | BytesIO:
+        pass
+
     @abstractmethod
-    def _read_raw():
+    def _read_raw(self) -> pd.DataFrame | pl.DataFrame | pa.Table:
+        pass
+
+    @abstractmethod
+    def _process_format():
+        pass
+
+    @abstractmethod
+    def _collect_metadata():
         pass
 
     @property
