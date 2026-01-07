4289c0a409b6ca7cddd5ac0c8abab13f9687c6e2
Simon Stensgaard <STcena1@hotmail.com>
2026-01-07 21:25:07 +0100

    Started working on _collect_history() method in BaseReader

--- src/reader/BaseReader.py
+++ src/reader/BaseReader.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import polars as pl
 import pyarrow as pa
+import json
 
 from src.utility import retrieve_output_format
 from src.utility.typings import DataTable, InputType
@@ -13,6 +14,7 @@ from typing import List, Union, Optional, Tuple, Any, Dict
 from abc import abstractmethod
 from dataclasses import field
 from datetime import datetime, timezone
+from time import perf_counter
 
 # ---------------------------------------------------------------
 # BASEREADER CLASS -> ABSTRACTION
@@ -35,7 +37,19 @@ class BaseReader():
         self,
         input: InputType,
     ) -> DataTable:
-        pass
+        start_time = perf_counter()
+
+        raw_data = self._read_raw(input)
+        processed_data = self._process_format(raw_data)
+
+        if self.collect_metadata:
+            self._collect_metadata(processed_data)
+
+        end_stime = perf_counter()
+        elapsed_time = end_stime - start_time
+        self._collect_history(elapsed_time)
+
+        return processed_data
 
     def _handle_input(
         self,
@@ -43,16 +57,25 @@ class BaseReader():
     ) -> DataTable:
         pass
 
+    def _collect_history(self, elapsed_time: float) -> None:
+        information_dict = {
+            "reader_id": id(self),
+            "timestamp": datetime.now(timezone.utc).isoformat(),
+            "elapsed_time": elapsed_time,
+        }
+
+        self.history.append(json.dumps(information_dict))
+
     @abstractmethod
     def _read_raw(self) -> DataTable:
         pass
 
     @abstractmethod
-    def _process_format():
+    def _process_format(self):
         pass
 
     @abstractmethod
-    def _collect_metadata():
+    def _collect_metadata(self):
         pass
 
     @property
