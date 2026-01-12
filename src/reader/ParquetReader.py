09654a156bd067d555aa79478e9e7d7cfff092b1
Simon Stensgaard <STcena1@hotmail.com>
2026-01-12 21:39:33 +0100

    Configured _read_raw() method in Parquet Reader class

--- src/reader/ParquetReader.py
+++ src/reader/ParquetReader.py
@@ -20,20 +20,60 @@ from src.utility.typings import DataTable, InputType
 
 class ParquetReader(BaseReader):
 
-    __slots__ = ()
+    __slots__ = (
+        "columns",
+        "use_threads",
+        "engine",
+        "filters",
+        "n_rows",
+    )
 
-    def __init__(self, metadata = True):
-        super().__init__(metadata)
+    def __init__(
+        self,
+        *,
+        columns: Optional[List[str]] = None,
+        use_threads: bool = True,
+        engine: str = "pyarrow",
+        filters: Optional[List[Tuple[str, str, Any]]] = None,
+        n_rows: Optional[int] = None,
+        **base_kwargs,
+    ):
+        super().__init__(**base_kwargs)
+
+        self.columns = columns
+        self.use_threads = use_threads
+        self.engine = engine
+        self.filters = filters
+        self.n_rows = n_rows
+
 
     def _read_raw(self, input: InputType, engine: str = "pandas") -> DataTable:
-        super()._read_raw(input, engine)
-
-        match engine:
-            case "pandas":
-                df = pd.read_parquet(input)
-            case "polars":
-                df = pl.read_parquet(input)
-            case "pyarrow":
-                df = pa.parquet.read_table(input)
+        if engine == "pandas":
+            df = pd.read_parquet(
+                input,
+                columns=self.columns,
+                engine=self.engine,
+                filters=self.filters,
+                use_nullable_dtypes=True,
+            )
+            if self.n_rows:
+                df = df.head(self.n_rows)
+        elif engine == "polars":
+            df = pl.read_parquet(
+                input,
+                columns=self.columns,
+                use_threads=self.use_threads,
+                filters=self.filters,
+            )
+        elif engine == "pyarrow":
+            df = pa.parquet.read_table(
+                input,
+                columns=self.columns,
+                use_threads=self.use_threads,
+                filters=self.filters,
+            )
+        else:
+            self.logger.error(f"Unsupported engine: {engine}")
+            raise ValueError(f"Unsupported engine: {engine}")
 
         return df
