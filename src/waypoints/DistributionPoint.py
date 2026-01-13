c6c7a419147858cffd1602ae6f82f737c77094c9
Simon Stensgaard <STcena1@hotmail.com>
2026-01-13 21:17:28 +0100

    Added the general Waypoint-classes and started to configure them

---
+++ src/waypoints/DistributionPoint.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------------
+# IMPORTS
+# ---------------------------------------------------------------
+
+from typing import List, Union, Optional, Tuple, Any
+from datetime import datetime, timezone
+
+# ---------------------------------------------------------------
+# DISTRIBUTIONPOINT CLASS -> EXTENSION OF BASEPOINT
+# ---------------------------------------------------------------
