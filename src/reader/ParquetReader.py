38cd0744fc5fe10f4535081bb1cf3214e7d36b22
Simon Stensgaard <STcena1@hotmail.com>
2026-01-20 21:39:29 +0100

    Configured ParquetReader subclass and functionality

--- src/reader/ParquetReader.py
+++ src/reader/ParquetReader.py
@@ -2,14 +2,12 @@
 # IMPORTS
 # ---------------------------------------------------------------
 
-import pandas as pd
 import polars as pl
-import pyarrow as pa
 
 from typing import List, Optional, Tuple, Any
 
 from src.reader import BaseReader
-from src.utility.typings import DataTable, InputType
+from src.typings import ReaderConfig, InputType
 
 # ---------------------------------------------------------------
 # PARQUETREADER CLASS
@@ -18,59 +16,64 @@ from src.utility.typings import DataTable, InputType
 class ParquetReader(BaseReader):
 
     __slots__ = (
-        "columns",
-        "use_threads",
-        "engine",
-        "filters",
         "n_rows",
+        "row_index_name",
+        "rechunk",
+        "low_memory",
     )
 
     def __init__(
         self,
         *,
-        columns: Optional[List[str]] = None,
-        use_threads: bool = True,
-        engine: str = "pyarrow",
-        filters: Optional[List[Tuple[str, str, Any]]] = None,
         n_rows: Optional[int] = None,
+        row_index_name: Optional[str] = None,
+        rechunk: bool = False,
+        low_memory: bool = False,
+        verbosity: int = 0,
         **base_kwargs,
     ):
-        super().__init__(**base_kwargs)
+        super().__init__(verbosity=verbosity, **base_kwargs)
 
-        self.columns = columns
-        self.use_threads = use_threads
-        self.engine = engine
-        self.filters = filters
         self.n_rows = n_rows
+        self.row_index_name = row_index_name
+        self.rechunk = rechunk
+        self.low_memory = low_memory
 
+    def _materialize_config(self) -> ReaderConfig:
+        return ReaderConfig(
+            parameters={
+                "n_rows": self.n_rows,
+                "row_index_name": self.row_index_name,
+                "rechunk": self.rechunk,
+                "low_memory": self.low_memory,
+            }
+        )
+    
+    def _discover_schema(self, input: InputType) -> pl.Schema:
+        
+        lf = pl.scan_parquet(
+            source=input,
+            n_rows=self.n_rows,
+            row_index_name=self.row_index_name,
+            rechunk=self.rechunk,
+            low_memory=self.low_memory,
+        )
 
-    def _read_raw(self, input: InputType, engine: str = "pandas") -> DataTable:
-        if engine == "pandas":
-            df = pd.read_parquet(
-                input,
-                columns=self.columns,
-                engine=self.engine,
-                filters=self.filters,
-                use_nullable_dtypes=True,
-            )
-            if self.n_rows:
-                df = df.head(self.n_rows)
-        elif engine == "polars":
-            df = pl.read_parquet(
-                input,
-                columns=self.columns,
-                use_threads=self.use_threads,
-                filters=self.filters,
-            )
-        elif engine == "pyarrow":
-            df = pa.parquet.read_table(
-                input,
-                columns=self.columns,
-                use_threads=self.use_threads,
-                filters=self.filters,
-            )
-        else:
-            self.logger.error(f"Unsupported engine: {engine}")
-            raise ValueError(f"Unsupported engine: {engine}")
+        schema = lf.schema
+        self._logger(f"Schema initialized: {schema}")
 
-        return df
+        return schema
+    
+    def _to_lazyframe(self, input: InputType) -> pl.LazyFrame:
+        
+        lf = pl.scan_parquet(
+            source=input,
+            n_rows=self.n_rows,
+            row_index_name=self.row_index_name,
+            rechunk=self.rechunk,
+            low_memory=self.low_memory,
+        )
+
+        self._logger(f"Data succesfully loaded into LazyFrame.")
+        return lf
+
